14afacea219433a6353c707468f1bd9d35a58f43
Joyce <lapinette2002@ymail.com>
2025-04-01 16:00:02 +0200

    Update installation command in start.bash to allow breaking system packages

--- POCs/IR/start.bash
+++ POCs/IR/start.bash
@@ -1,5 +1,5 @@
 sudo apt-get update
 sudo apt-get install python3 python3-pip python3-opencv gstreamer1.0-tools
-pip3 install opencv-python numpy
+pip3 install opencv-python numpy --break-system-packages
 libcamera-hello
 python3 main.py
