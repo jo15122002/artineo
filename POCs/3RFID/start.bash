9cc7d23050a89faa18c0395071d1066929ea475b
Joyce <lapinette2002@ymail.com>
2025-04-02 15:13:19 +0200

    Refactor start.bash: streamline virtual environment setup and dependency installation process

--- POCs/3RFID/start.bash
+++ POCs/3RFID/start.bash
@@ -3,43 +3,21 @@
 # Se positionner dans le répertoire du script
 cd "$(dirname "$0")"
 
-# Vérifier et activer l'interface SPI dans /boot/config.txt
-echo "Vérification de l'activation de SPI dans /boot/config.txt..."
-if ! grep -q "^dtparam=spi=on" /boot/config.txt; then
-    echo "SPI non activé, modification de /boot/config.txt..."
-    # Décommente la ligne si elle est présente en commentaire
-    sudo sed -i 's/^#dtparam=spi=on/dtparam=spi=on/' /boot/config.txt
-    # Si la ligne n'est toujours pas présente, on l'ajoute à la fin du fichier
-    if ! grep -q "^dtparam=spi=on" /boot/config.txt; then
-        echo "dtparam=spi=on" | sudo tee -a /boot/config.txt
-    fi
-    echo "SPI a été activé dans /boot/config.txt."
-    echo "Pour appliquer ces changements, un redémarrage est nécessaire."
-    read -p "Voulez-vous redémarrer maintenant ? [O/n] " response
-    if [[ "$response" =~ ^[Oo] ]]; then
-       sudo reboot
-       exit 0
-    else
-       echo "N'oubliez pas de redémarrer votre Raspberry Pi plus tard."
-    fi
-else
-    echo "SPI est déjà activé."
-fi
-
 # Vérifier si l'environnement virtuel existe, sinon le créer
 if [ ! -d "venv" ]; then
     echo "Création de l'environnement virtuel..."
     python3 -m venv venv
-    echo "Activation de l'environnement virtuel..."
-    source venv/bin/activate
-    echo "Installation des dépendances..."
-    pip install --upgrade pip
-    pip install -r requirements.txt
-else
-    echo "Activation de l'environnement virtuel..."
-    source venv/bin/activate
-    pip install -r requirements.txt
 fi
 
-echo "Lancement du programme RFID..."
+# Activation de l'environnement virtuel
+echo "Activation de l'environnement virtuel..."
+source venv/bin/activate
+
+# Mise à jour de pip et installation des dépendances
+echo "Installation des dépendances..."
+pip install --upgrade pip
+pip install -r requirements.txt
+
+# Lancement du script principal main.py
+echo "Démarrage de main.py..."
 python3 main.py
