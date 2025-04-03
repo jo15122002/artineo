82baa66a77dc3bd020f541af831176b6764fa7d7
Joyce <lapinette2002@ymail.com>
2025-04-03 15:00:40 +0200

    Enhance RFID reader functionality by adding support for a second reader and improving output messages

--- POCs/3RFID/main.py
+++ POCs/3RFID/main.py
@@ -1,35 +1,43 @@
 from machine import SPI, Pin
 from utime import sleep
-
 import mfrc522
 
-
 def main():
-    # Configuration SPI pour ESP32 :
-    # Vous pouvez adapter les pins en fonction de votre câblage.
-    # Ici on utilise SPI(1) avec une vitesse de 2.5MHz.
-    spi = SPI(2, baudrate=2500000, polarity=0, phase=0, sck=Pin(12), mosi=Pin(23), miso=Pin(13))
+    # Configuration SPI pour ESP32
+    spi = SPI(2, baudrate=2500000, polarity=0, phase=0,
+              sck=Pin(12), mosi=Pin(23), miso=Pin(13))
     spi.init()
     
-    # Initialisation du lecteur RFID.
-    # Dans cet exemple, gpioRst est connecté à GPIO0 et gpioCs à GPIO2.
-    rdr = mfrc522.MFRC522(spi=spi, gpioRst=4, gpioCs=5)
+    # Initialisation du Lecteur 1 (RST: GPIO4, CS: GPIO5)
+    rdr1 = mfrc522.MFRC522(spi=spi, gpioRst=4, gpioCs=5)
+    # Initialisation du Lecteur 2 (RST: GPIO16, CS: GPIO17)
+    rdr2 = mfrc522.MFRC522(spi=spi, gpioRst=16, gpioCs=17)
     
-    print("Placez une carte RFID près du lecteur...")
+    print("Placez une carte RFID près de l'un des lecteurs...")
     
     while True:
-        # Envoyer une requête pour détecter une carte (REQIDL)
-        stat, tag_type = rdr.request(rdr.REQIDL)
-        if stat == rdr.OK:
-            # Exécute la procédure d'anticollision pour récupérer l'UID
-            stat, raw_uid = rdr.anticoll()
-            if stat == rdr.OK:
-                print("Carte détectée!")
-                print("Type: 0x%02x" % tag_type)
-                print("UID: 0x%02x%02x%02x%02x" % (raw_uid[0], raw_uid[1], raw_uid[2], raw_uid[3]))
+        # Vérifier le Lecteur 1
+        stat1, tag_type1 = rdr1.request(rdr1.REQIDL)
+        if stat1 == rdr1.OK:
+            stat1, raw_uid1 = rdr1.anticoll()
+            if stat1 == rdr1.OK:
+                print("Lecteur 1 : Carte détectée !")
+                print("Type: 0x%02x" % tag_type1)
+                print("UID: 0x%02x%02x%02x%02x" % (raw_uid1[0], raw_uid1[1], raw_uid1[2], raw_uid1[3]))
                 print("")
-                # Attendre 2 secondes pour éviter des lectures multiples consécutives
-                sleep(2)
+                sleep(2)  # Attendre pour éviter des lectures multiples consécutives
+
+        # Vérifier le Lecteur 2
+        stat2, tag_type2 = rdr2.request(rdr2.REQIDL)
+        if stat2 == rdr2.OK:
+            stat2, raw_uid2 = rdr2.anticoll()
+            if stat2 == rdr2.OK:
+                print("Lecteur 2 : Carte détectée !")
+                print("Type: 0x%02x" % tag_type2)
+                print("UID: 0x%02x%02x%02x%02x" % (raw_uid2[0], raw_uid2[1], raw_uid2[2], raw_uid2[3]))
+                print("")
+                sleep(2)  # Attendre pour éviter des lectures multiples consécutives
+        
         sleep(0.1)
 
 if __name__ == '__main__':
