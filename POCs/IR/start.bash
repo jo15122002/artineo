30b6318d8c6ae5ca2ff68191f7990e6ffcd72118
Joyce <lapinette2002@ymail.com>
2025-04-06 13:23:17 +0200

    Enhance start.bash by adding additional GStreamer installation commands for improved video processing capabilities

--- POCs/IR/start.bash
+++ POCs/IR/start.bash
@@ -1,6 +1,12 @@
 sudo apt-get update
+sudo apt-get install python3 python3-pip python3-opencv gstreamer1.0-tools
 sudo apt-get install gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly
+sudo apt-get install gstreamer1.0-libav gstreamer1.0-doc gstreamer1.0-rtsp gstreamer1.0-rtsp-server
+sudo apt-get install gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-gtk3 gstreamer1.0-x gstreamer1.0-pulseaudio
+sudo apt-get install gstreamer1.0-omx gstreamer1.0-omx-rpi gstreamer1.0-omx-rpi-config
+sudo apt-get install gstreamer1.0-omx-rpi-dev gstreamer1.0-omx-rpi-doc gstreamer1.0-omx-rpi-examples
+sudo apt-get install gstreamer1.0-omx-rpi-plugins gstreamer1.0-omx-rpi-plugins-doc gstreamer1.0-omx-rpi-plugins-examples
+sudo apt-get install gstreamer1.0-omx-rpi-plugins-dev gstreamer1.0-omx-rpi-plugins-doc gstreamer1.0-omx-rpi-plugins-examples
 pip3 install opencv-python numpy --break-system-packages
 libcamera-hello
-gst-launch-1.0 libcamerasrc ! video/x-raw,width=640,height=480,framerate=30/1 ! videoconvert ! autovideosink
 python3 main.py
