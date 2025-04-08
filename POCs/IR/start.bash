6f5282f48b1037c98a09a47a911a61708830af69
Joyce <lapinette2002@ymail.com>
2025-04-08 19:39:12 +0200

    Add v4l2loopback installation and configuration for virtual camera support

--- POCs/IR/start.bash
+++ POCs/IR/start.bash
@@ -10,3 +10,11 @@ sudo apt-get install -y \
 libcamera-hello -t 5000 --nopreview
 
 v4l2-ctl --list-devices
+
+sudo apt-get install v4l2loopback-dkms
+
+sudo modprobe v4l2loopback video_nr=2 card_label="CameraLoop" exclusive_caps=1
+
+libcamera-vid -t 0 --nopreview --inline --output /dev/video2
+
+gst-launch-1.0 v4l2src device=/dev/video2 ! "video/x-raw,width=1296,height=972,framerate=30/1" ! videoscale ! "video/x-raw,width=320,height=240" ! videoconvert ! autovideosink
