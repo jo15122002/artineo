83a26a42a9d0242e7ea45c9a9de3857cff8dd2be
Joyce <lapinette2002@ymail.com>
2025-04-08 19:58:03 +0200

    Fix v4l2loopback module loading and update video encoding parameters in start.bash

--- POCs/IR/start.bash
+++ POCs/IR/start.bash
@@ -13,8 +13,9 @@ v4l2-ctl --list-devices
 
 sudo apt-get install v4l2loopback-dkms
 
-sudo modprobe v4l2loopback video_nr=2 card_label="CameraLoop" exclusive_caps=1
+sudo modprobe -r v4l2loopback
+sudo modprobe v4l2loopback video_nr=2 card_label="CameraLoop" exclusive_caps=1 max_buffers=4
 
-libcamera-vid -t 0 --nopreview --inline --output /dev/video2
+libcamera-vid -t 0 --nopreview --inline --codec yuv420 --width 1296 --height 972 --output /dev/video2
 
-gst-launch-1.0 v4l2src device=/dev/video2 ! "video/x-raw,width=1296,height=972,framerate=30/1" ! videoscale ! "video/x-raw,width=320,height=240" ! videoconvert ! autovideosink
+gst-launch-1.0 v4l2src device=/dev/video2 ! "video/x-raw,format=I420,width=1296,height=972,framerate=30/1" ! videoscale ! "video/x-raw,width=320,height=240" ! videoconvert ! autovideosink
