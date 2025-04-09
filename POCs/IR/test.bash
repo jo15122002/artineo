cc61b1935952c833318e983592fc5c443ec59106
Joyce <lapinette2002@ymail.com>
2025-04-09 20:38:18 +0200

    Replace cam_debug.py with main.py in test.bash for streamlined camera streaming

--- POCs/IR/test.bash
+++ POCs/IR/test.bash
@@ -21,6 +21,6 @@ fi
 echo "Lancement de libcamera-vid et envoi du flux à cam_debug.py..."
 libcamera-vid -t 0 --width 640 --height 480 --inline --codec yuv420 --output - | \
 ffmpeg -loglevel error -f rawvideo -pix_fmt yuv420p -s 640x480 -i - -f rawvideo -pix_fmt bgr24 - | \
-python3 cam_debug.py
+python3 main.py
 
 echo "Script terminé."
