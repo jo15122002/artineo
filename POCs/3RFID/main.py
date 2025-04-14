6352ce73123bd512c9b84a9780fb618b27ee62a9
Joyce <lapinette2002@ymail.com>
2025-04-14 14:42:05 +0200

    Refactor RFID reader setup and button handling; improve LED feedback for UID verification

--- POCs/3RFID/main.py
+++ POCs/3RFID/main.py
@@ -1,10 +1,24 @@
+import neopixel
 from machine import SPI, Pin
 from utime import sleep
-import mfrc522
-import neopixel
 
-def main():
-    # Configuration SPI pour l'ESP32
+import mfrc522
+
+# Flag global pour l'appui du bouton
+button_pressed = False
+
+def button_irq_handler(pin):
+    global button_pressed
+    # Dès que le bouton est appuyé (front descendant), définir le flag.
+    button_pressed = True
+
+def setup():
+    global rdr1, rdr2, rdr3, led1, led2, led3, button
+    global last_uid1, last_uid2, last_uid3
+    global expected_uid1, expected_uid2, expected_uid3
+
+    print("Setup...")
+    # Configuration SPI pour ESP32 : ajustez les pins selon votre câblage
     spi = SPI(2, baudrate=2500000, polarity=0, phase=0,
               sck=Pin(12), mosi=Pin(23), miso=Pin(13))
     spi.init()
@@ -14,113 +28,127 @@ def main():
     rdr2 = mfrc522.MFRC522(spi=spi, gpioRst=16, gpioCs=17)
     rdr3 = mfrc522.MFRC522(spi=spi, gpioRst=25, gpioCs=26)
     
-    # Initialisation des LED pour chaque lecteur RFID
+    # Initialisation des LED WS2812b pour chaque lecteur RFID
     led1 = neopixel.NeoPixel(Pin(15), 1)
     led2 = neopixel.NeoPixel(Pin(18), 1)
     led3 = neopixel.NeoPixel(Pin(19), 1)
-    # Éteindre les LED initialement
     for led in (led1, led2, led3):
         led[0] = (0, 0, 0)
         led.write()
     
-    # # Initialisation du bouton sur le pin 21 avec résistance pull-down
-    button = Pin(14, Pin.IN)
+    # Configuration du bouton sur le GPIO14 en mode pull-up.
+    # Le bouton doit relier la broche à 0 quand il est appuyé.
+    button = Pin(14, Pin.IN, Pin.PULL_UP)
+    button.irq(trigger=Pin.IRQ_FALLING, handler=button_irq_handler)
     
-    # Variables pour stocker le dernier UID lu pour chaque lecteur (sous forme de string)
+    # Variables pour stocker les UID lus par chaque lecteur (en string)
     last_uid1 = None
     last_uid2 = None
     last_uid3 = None
     
-    # UID attendus sous forme de chaîne (sans séparateurs, en minuscules)
+    # UID attendus (en minuscules, sans séparateurs)
     expected_uid1 = "e7f000a9be"
     expected_uid2 = "ea1f11b054"
     expected_uid3 = "b66640c151"
     
-    print("Placez une carte RFID près d'un des lecteurs...")
+    print("Placez une carte RFID et appuyez sur le bouton pour lancer la lecture.")
+
+def main():
+    global last_uid1, last_uid2, last_uid3, button_pressed
+    global expected_uid1, expected_uid2, expected_uid3
+    setup()
     
     while True:
-        # print(button.value())
-
-        # Vérification continue du lecteur 1
-        stat1, tag_type1 = rdr1.request(rdr1.REQIDL)
-        if stat1 == rdr1.OK:
-            stat1, raw_uid1 = rdr1.anticoll()
+        # Attendre que le bouton soit appuyé
+        print("En attente d'appui sur le bouton...")
+        if button_pressed:
+            # Réinitialiser le flag
+            button_pressed = False
+            
+            # Lire le Lecteur 1
+            stat1, tag_type1 = rdr1.request(rdr1.REQIDL)
             if stat1 == rdr1.OK:
-                # Conversion de la liste d'octets en string hexadécimal
-                last_uid1 = "".join("{:02x}".format(x) for x in raw_uid1)
-                print("Lecteur 1 : Carte détectée, UID :", last_uid1)
-                sleep(0.5)
-                
-        # Vérification continue du lecteur 2
-        stat2, tag_type2 = rdr2.request(rdr2.REQIDL)
-        if stat2 == rdr2.OK:
-            stat2, raw_uid2 = rdr2.anticoll()
+                stat1, raw_uid1 = rdr1.anticoll()
+                if stat1 == rdr1.OK:
+                    last_uid1 = "".join("{:02x}".format(x) for x in raw_uid1)
+                else:
+                    last_uid1 = None
+            else:
+                last_uid1 = None
+            
+            # Lire le Lecteur 2
+            stat2, tag_type2 = rdr2.request(rdr2.REQIDL)
             if stat2 == rdr2.OK:
-                last_uid2 = "".join("{:02x}".format(x) for x in raw_uid2)
-                print("Lecteur 2 : Carte détectée, UID :", last_uid2)
-                # sleep(0.5)
-                
-        # Vérification continue du lecteur 3
-        stat3, tag_type3 = rdr3.request(rdr3.REQIDL)
-        if stat3 == rdr3.OK:
-            stat3, raw_uid3 = rdr3.anticoll()
+                stat2, raw_uid2 = rdr2.anticoll()
+                if stat2 == rdr2.OK:
+                    last_uid2 = "".join("{:02x}".format(x) for x in raw_uid2)
+                else:
+                    last_uid2 = None
+            else:
+                last_uid2 = None
+
+            # Lire le Lecteur 3
+            stat3, tag_type3 = rdr3.request(rdr3.REQIDL)
             if stat3 == rdr3.OK:
-                last_uid3 = "".join("{:02x}".format(x) for x in raw_uid3)
-                print("Lecteur 3 : Carte détectée, UID :", last_uid3)
-                # sleep(0.5)
-        
-        # Vérification à l'appui du bouton
-        if button.value() == 0:
+                stat3, raw_uid3 = rdr3.anticoll()
+                if stat3 == rdr3.OK:
+                    last_uid3 = "".join("{:02x}".format(x) for x in raw_uid3)
+                else:
+                    last_uid3 = None
+            else:
+                last_uid3 = None
+
             print("Bouton appuyé, vérification des UID...")
-            # Lecteur 1
+            # Vérifier Lecteur 1 et allumer la LED correspondante
             if last_uid1 is None:
                 led1[0] = (255, 165, 0)  # Orange
-                print("Lecteur 1 : Aucune carte détectée, LED orange")
+                print("Lecteur 1 : Aucune carte détectée")
             elif last_uid1 == expected_uid1:
                 led1[0] = (0, 255, 0)    # Vert
-                print("Lecteur 1 : UID correct, LED verte")
+                print("Lecteur 1 : UID correct")
             else:
                 led1[0] = (255, 0, 0)    # Rouge
-                print("Lecteur 1 : UID incorrect, LED rouge")
+                print("Lecteur 1 : UID incorrect")
             led1.write()
             
-            # Lecteur 2
+            # Vérifier Lecteur 2
             if last_uid2 is None:
                 led2[0] = (255, 165, 0)  # Orange
-                print("Lecteur 2 : Aucune carte détectée, LED orange")
+                print("Lecteur 2 : Aucune carte détectée")
             elif last_uid2 == expected_uid2:
                 led2[0] = (0, 255, 0)    # Vert
-                print("Lecteur 2 : UID correct, LED verte")
+                print("Lecteur 2 : UID correct")
             else:
                 led2[0] = (255, 0, 0)    # Rouge
-                print("Lecteur 2 : UID incorrect, LED rouge")
+                print("Lecteur 2 : UID incorrect")
             led2.write()
             
-            # Lecteur 3
+            # Vérifier Lecteur 3
             if last_uid3 is None:
                 led3[0] = (255, 165, 0)  # Orange
-                print("Lecteur 3 : Aucune carte détectée, LED orange")
+                print("Lecteur 3 : Aucune carte détectée")
             elif last_uid3 == expected_uid3:
                 led3[0] = (0, 255, 0)    # Vert
-                print("Lecteur 3 : UID correct, LED verte")
+                print("Lecteur 3 : UID correct")
             else:
                 led3[0] = (255, 0, 0)    # Rouge
-                print("Lecteur 3 : UID incorrect, LED rouge")
+                print("Lecteur 3 : UID incorrect")
             led3.write()
             
-            # Attente que le bouton soit relâché pour éviter les déclenchements multiples
+            # Attendre que le bouton soit relâché pour éviter plusieurs déclenchements
             while button.value() == 0:
-                sleep(0.1)
+                sleep(0.01)
             
-            # Optionnel : remettre les LED à l'état éteint après quelques secondes
-            # sleep(2)
+            # Optionnel : éteindre les LED après 2 secondes
+            sleep(2)
             for led in (led1, led2, led3):
                 led[0] = (0, 0, 0)
                 led.write()
                 
-        sleep(0.1)
+        sleep(0.05)
 
 if __name__ == "__main__":
+    print("Démarrage du programme...")
     try:
         main()
     except KeyboardInterrupt:
