772c7fd059c5282dea1198f06209c376a7661785
Joyce <lapinette2002@ymail.com>
2025-05-03 21:44:03 +0200

    poc application affichage act 1 IR

---
+++ web/pages/modules/module1.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="module1-container">
+      <img
+        v-if="backgroundPath"
+        :src="`${apiBase}/getAsset?module=1&path=${backgroundPath}`"
+        class="fullscreen-img"
+        :style="{ filter: filterStyle }"
+      />
+    </div>
+  </template>
+  
+  <script setup>
+  import { useRuntimeConfig } from '#app'
+import use1ir from '~/composables/module1'
+  
+  definePageMeta({ layout: 'module' })
+  
+  const { public: { apiBase } } = useRuntimeConfig()
+  const { backgroundPath, filterStyle } = use1ir()
+  </script>
+  
+  <style scoped src="~/assets/modules/1/style.css"></style>
+  
