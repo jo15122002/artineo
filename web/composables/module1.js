2789a43840162d2d9dd4543813ec669d29d02499
Joyce <lapinette2002@ymail.com>
2025-05-03 21:49:21 +0200

    fix: update default realDiameter value to 6 cm

--- web/composables/module1.js
+++ web/composables/module1.js
@@ -7,7 +7,7 @@ export default function use1ir() {
     const serverUrl = apiBase
 
     // paramètres calibrage reçus depuis le serveur
-    const realDiameter = ref(1.5)    // en cm, valeur par défaut
+    const realDiameter = ref(6)    // en cm, valeur par défaut
     const focalLength = ref(400)     // en pixels, valeur par défaut
     const backgroundPath = ref('painting.jpeg')   // chemin du PNG plein écran
 
