edd23944c61a0aeb4183aa4c0fba9b610eb6fd7c
Joyce <lapinette2002@ymail.com>
2025-05-03 11:15:54 +0200

    isolation par modules

---
+++ web/layouts/module.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="module-container">
+      <slot/>
+    </div>
+  </template>
+  
+  <script setup>
+  definePageMeta({ layout: false })
+  </script>
+  
+  <style>
+  html, body, #__nuxt, .module-container {
+    margin: 0; padding: 0; height: 100%;
+    width: 100%;
+    overflow: hidden;
+  }
+  </style>
+  
