10f3564d3d6269ec5475fa5a2b28a8a153a387a0
Joyce <lapinette2002@ymail.com>
2025-06-12 22:11:40 +0200

    merci tutur

--- serveur/front/pages/modules/module2.vue
+++ serveur/front/pages/modules/module2.vue
@@ -1,6 +1,31 @@
 <template>
   <div class="module2-container">
-    <canvas ref="canvas" />
+    <div class="frame-and-buttons-wrapper">
+      <div class="frame-shadow">
+        <div class="frame">
+          <canvas ref="canvas" />
+        </div>
+      </div>
+      <div class="buttons-wrapper">
+        <div class="button rectX-button">
+          <img src="~/assets/modules/2/rectX.svg" alt="Rect X Button" />
+          <div class="rect-selector"
+               :style="{ transform: `translateX(calc(${percentageX} * 193px))` }"
+          />
+        </div>
+        <div class="button rectY-button">
+          <img src="~/assets/modules/2/rectY.svg" alt="Rect Y Button" />
+          <div class="rect-selector"
+                :style="{ transform: `translateX(calc(${percentageY} * -143px))` }"
+          />
+        </div>
+        <div class="button circle-button">
+          <img src="~/assets/modules/2/circle.svg" alt="Circle Button" />
+          <div class="rect-selector"
+          :style="{ transform: `translateY(-115px)`, rotate: `calc(${percentageRotate} * 180deg)` }"/>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -13,6 +38,10 @@ definePageMeta({ layout: 'module' })
 
 // 1) On crée la ref vers le <canvas>
 const canvas = ref<HTMLCanvasElement | null>(null)
+
+const percentageX = ref(0.2) // Valeur entre -1 et 1 pour la position X du rectangle
+const percentageY = ref(0.2) // Valeur entre -1 et 1 pour la position Y du rectangle
+const percentageRotate = ref(0.2) // Valeur entre -1 et 1 pour la rotation du cercle
 
 // 2) On appelle le composable :
 //    il va initialiser Three.js, charger OBJ/MTL, démarrer WS/polling, etc.
